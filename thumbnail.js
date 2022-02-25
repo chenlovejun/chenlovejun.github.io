38fdc8dae5fbf723951c767d85e0ae6a6e953890
chenlovejun <100207744+chenlovejun@users.noreply.github.com>
2022-02-26 02:09:07 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,7 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 let targetImage = document.querySelector("#smart-image");
 targetImage.addEventListener("click", function() {
-    alert("点击了图片");
-    targetImage.classList.remove("small");
+alert("点击了图片");
+if (targetImage.classList.contains("small")) {
+  remove("small");
+} else {
+  add("small");    
+}
 });
 });
